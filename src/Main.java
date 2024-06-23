6c8341c2fd18b9e6c53f54161a8cce92f3b3fe66
Sobh6n <sobhankhedry7297@gmail.com>
2024-06-23 16:24:38 +0330

    created

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+class Activity{
+    private int i;
+    private int Pi;
+    private int Di;
+    public Activity(int i , int Pi, int Di){
+        this.i = i;
+        this.Pi =Pi;
+        this.Di = Di;
+    }
+
+    public int getDi() {
+        return Di;
+    }
+
+    public int getI() {
+        return i;
+    }
+
+    public int getPi() {
+        return Pi;
+    }
+}
